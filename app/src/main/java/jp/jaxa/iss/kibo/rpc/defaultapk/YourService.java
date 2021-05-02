2058a6b20746bbc44319166aea421fa8905c8b80
Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2021-05-03 00:39:52 +0700

    feat: Interpret QR string and move to point A'

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -1,6 +1,7 @@
 package jp.jaxa.iss.kibo.rpc.defaultapk;
 
 // android
+
 import android.graphics.Bitmap;
 import android.util.Log;
 // kibo rpc api
@@ -8,46 +9,69 @@ import gov.nasa.arc.astrobee.Result;
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
 // astrobee gs
 import gov.nasa.arc.astrobee.types.*;
-import gov.nasa.arc.astrobee.*;
 // zxing
+import com.google.zxing.BarcodeFormat;
 import com.google.zxing.BinaryBitmap;
+import com.google.zxing.DecodeHintType;
 import com.google.zxing.LuminanceSource;
+import com.google.zxing.qrcode.QRCodeReader;
 import com.google.zxing.RGBLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
-import com.google.zxing.qrcode.QRCodeReader;
 // opencv
 import org.opencv.android.Utils;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Rect;
 import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
 
 
 public class YourService extends KiboRpcService {
     @Override
-    protected void runPlan1(){
+    protected void runPlan1() {
         api.startMission();
 
         move_to(11.2100, -9.8000, 4.7900, 0, 0, -0.7070f, 0.7070f);
 
-        qr_move();
+        String qrContents = qr_move();
+        float qrData[] = interpretQRString(qrContents); // {kozPattern, x, y, z}
+        Log.i("Interpret QR String", Arrays.toString(qrData));
+
+        int pattern = (int)qrData[0];
+        move_to(qrData[1], qrData[2], qrData[3], 0, 0, -0.707f, 0.707f);
 
         api.takeSnapshot();
         api.reportMissionCompletion();
     }
 
     @Override
-    protected void runPlan2(){
+    protected void runPlan2() {
         // write here your plan 2
     }
 
     @Override
-    protected void runPlan3(){
+    protected void runPlan3() {
         // write here your plan 3
     }
 
     final int LOOP_MAX = 3;
     final int NAV_MAX_WIDTH = 1280;
     final int NAV_MAX_HEIGHT = 960;
+    final double[] CAM_MATSIM = {
+            567.229305, 0.0, 659.077221,
+            0.0, 574.192915, 517.007571,
+            0.0, 0.0, 1.0
+    };
+
+    final double[] DIST_COEFFSIM = {
+            -0.216247, 0.03875, -0.010157, 0.001969, 0.0
+    };
+
 
     public void log_kinematics() {
         final String TAG = "log_position";
@@ -77,129 +101,104 @@ public class YourService extends KiboRpcService {
         Log.i(TAG, "Done");
     }
 
-    /*
-    public Mat getCamMat(double[] in) {
-        Mat out = new Mat(3, 3, CvType.CV_32FC1);
-        int k = 0, j = 0;
-        for (int i = 0; i < in.length; i++) {
-            out.put(k, j, in[i]);
-
-            j++;
-            if (j == 2) {
-                k++;
-                j = 0;
-            }
-        }
-        return out;
-    }
-
-    public Mat getDistCoeff(double[] in) {
-        Mat out = new Mat(1, 5, CvType.CV_32FC1);
-        for (int i = 0; i < in.length; i++) {
-            out.put(0, i, in[i]);
-        }
-        return out;
-    }
-     */
-
     public Mat undistort(Mat in) {
         final String TAG = "undistort";
-
-        /* generalize this func, should just declare some const for performance
-        double[][] cam_info = api.getNavCamIntrinsics();
-        Log.i(TAG, "cam matrix =" + Arrays.toString(cam_info[0]));
-        Log.i(TAG, "dist coeff =" + Arrays.toString(cam_info[1]));
-
-        Mat cam_Mat = getCamMat(cam_info[0]);
-        Mat dist_coeff = getDistCoeff(cam_info[1]);
-         */
+        Log.i(TAG, "Start");
 
         Mat cam_Mat = new Mat(3, 3, CvType.CV_32FC1);
         Mat dist_coeff = new Mat(1, 5, CvType.CV_32FC1);
 
-        final double cam_Mat_sim[] = {
-                567.229305, 0.0, 659.077221,
-                0.0, 574.192915, 517.007571,
-                0.0, 0.0, 1.0
-        };
-
-        final double dist_coeff_sim[] = {
-                -0.216247, 0.03875, -0.010157, 0.001969, 0.0
-        };
-
-        cam_Mat.put(0, 0, cam_Mat_sim);
-        dist_coeff.put(0, 0, dist_coeff_sim);
+        cam_Mat.put(0, 0, CAM_MATSIM);
+        dist_coeff.put(0, 0, DIST_COEFFSIM);
 
         Mat out = new Mat(NAV_MAX_WIDTH, NAV_MAX_HEIGHT, CvType.CV_8UC1);
+        Log.i(TAG, "Imgrpoc.undistrot");
         Imgproc.undistort(in, out, cam_Mat, dist_coeff);
 
+        Log.i(TAG, "Done");
         return out;
     }
 
+    public Rect crop() {
+        return new Rect(590, 480, 300, 400);
+    }
+
     public BinaryBitmap getNavImg() {
-        final String TAG = "procNavImg";
+        final String TAG = "getNavImg";
 
         // img processing shit
         Log.i(TAG, "Processing img");
 
-        api.flashlightControlFront(0.025f);
-        Mat pic = undistort(api.getMatNavCam());
-        api.flashlightControlFront(0);
+        Mat pic = new Mat(api.getMatNavCam(), crop());
 
-        Bitmap uncrop = Bitmap.createBitmap(pic.width(), pic.height(), Bitmap.Config.ARGB_8888);
-        Utils.matToBitmap(pic, uncrop);
+        Log.i(TAG, "bMap init");
+        Bitmap bMap = Bitmap.createBitmap(pic.width(), pic.height(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(pic, bMap);
 
-        Log.i(TAG, pic.width() + "");
-        Log.i(TAG, pic.height() + "");
-
-        Bitmap bMap = Bitmap.createBitmap(
-                uncrop,
-                pic.width()/2,
-                pic.width()/2,
-                (pic.width()/2) * (2/3),
-                (pic.height()/2) * (3/4));
-
-        // maybe need to resize or crop or some shit that idk to make it work.
-
+        Log.i(TAG, "intArr");
         int[] intArr = new int[bMap.getWidth() * bMap.getHeight()];
+
+        Log.i(TAG, "getPixels");
         bMap.getPixels(intArr, 0, bMap.getWidth(), 0, 0, bMap.getWidth(), bMap.getHeight());
+
+        Log.i(TAG, "Luminance Source");
         LuminanceSource source = new RGBLuminanceSource(bMap.getWidth(), bMap.getHeight(), intArr);
+
+        Log.i(TAG, "out");
         BinaryBitmap out = new BinaryBitmap(new HybridBinarizer(source));
 
         Log.i(TAG, "Success processing img");
         return out;
     }
 
-    public void qr_move() {
+    public String qr_move() {
         final String TAG = "qr_move";
         Log.i(TAG, "Start");
 
-        String data = null;
+        String text = null;
         int counter = 0;
-        BinaryBitmap bitmap = null;
+        BinaryBitmap bitmap;
 
-        while (data == null && counter < LOOP_MAX) {
+        Map<DecodeHintType, Object> hints = new Hashtable<>();
+        hints.put(DecodeHintType.TRY_HARDER, "");
+        List<BarcodeFormat> qr = new ArrayList<>(); qr.add(BarcodeFormat.QR_CODE);
+        hints.put(DecodeHintType.POSSIBLE_FORMATS, qr);
+        hints.put(DecodeHintType.CHARACTER_SET, "utf-8");
+
+        while (text == null && counter < LOOP_MAX) {
             try {
                 bitmap = getNavImg();
-            }
-            catch (Exception e) {
-                Log.i(TAG, "Failed processing img");
-                e.printStackTrace();
-                return ;
-            }
-
-            try {
                 // qr code reading
                 Log.i(TAG, "Reading qr code");
-                com.google.zxing.Result result = new QRCodeReader().decode(bitmap);
-                data = result.getText();
-                Log.i(TAG, "Data = " + data);
-            }
-            catch (Exception e) {
+                com.google.zxing.Result result = new QRCodeReader().decode(bitmap, hints);
+                text = result.getText();
+                api.sendDiscoveredQR(text);
+                Log.i(TAG, "Data = " + text);
+                Log.i(TAG, "Done -- Success");
+
+                return text;
+            } catch (Exception e) {
                 Log.i(TAG, "Failed reading qr code");
                 e.printStackTrace();
             }
+
+            counter++;
         }
 
+        Log.i(TAG, "Done with failure");
+        return null;
+    }
+
+    private float[] interpretQRString(String qrContents) {
+        Log.i("Interpret QR String", "Start");
+        String[] multi_contents = qrContents.split(",");
+
+        int pattern = Integer.parseInt(multi_contents[0].substring(5));
+
+        float final_x = Float.parseFloat(multi_contents[1].substring(4));
+        float final_y = Float.parseFloat(multi_contents[2].substring(4));
+        float final_z = Float.parseFloat(multi_contents[3].substring(4, multi_contents[3].length()-1));
+        Log.i("Interpret QR String", "Done");
+        return new float[] {pattern, final_x, final_y, final_z};
     }
 }
